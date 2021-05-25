c2316a9906ec5cfc0feb4e083ba271a5baaded1a
jeremy.laier <jeremylaier@outlook.com>
2021-05-24 21:56:12 -0500

    nyan

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import nyan from '../nyan.jpg';
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={nyan} alt="BigCo Inc. logo"/>
       </header>
     </div>
   );
