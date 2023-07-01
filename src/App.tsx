90331c07de5c729cf5a462cd62f89fa5b2c73a62
jeremylaier@outlook.com <jeremy@DESKTOP-QM9D71V>
2023-07-01 00:18:13 -0400

    add shrek gif and renmame files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import nyan from './200.gif';
+import shrek from './shrek.gif';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-      <img src={nyan} alt="BigCo Inc. logo"/>
+      <img src={nyan} alt="Dog gif"/>
+      <img src={shrek} alt="Shrek Movie"/>
 who was right
       </header>
     </div>
