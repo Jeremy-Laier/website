b5dbb9f42cd9c225ae86e75baf04b370ab0354bb
jeremylaier@outlook.com <jeremy@DESKTOP-QM9D71V>
2023-08-05 23:23:50 -0400

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import nyan from './200.gif';
-import shrek from './shrek.gif';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-      <img src={nyan} alt="Dog gif"/>
-      <img src={shrek} alt="Shrek Movie"/>
-who was right
+Welcome
       </header>
     </div>
   );
